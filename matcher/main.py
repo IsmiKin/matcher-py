dfe0c8f713338d3112187932c73e853ccb544fac
IsmiKin <ismikin@gmail.com>
2018-11-25 20:19:01 +0100

    Add loggin. Close #8

--- matcher/main.py
+++ matcher/main.py
@@ -1,14 +1,19 @@
 import time
+import logging
 
 from data.logic import get_all_sound_recordings
+
+FORMAT = "[%(asctime)s][%(levelname)-5.5s] %(message)s"
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.INFO)
 
 def main():
     while True:
         time.sleep(1)
         sound_recordings = get_all_sound_recordings()
         for record in sound_recordings:
-            print(record.id)
-
-        print("Hello World!")
+            # print(record.id)
+            log.info('PRINTING RECORD:{}'.format(record.id))
 
 main()
