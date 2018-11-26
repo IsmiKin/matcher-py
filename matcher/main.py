ce2ec3b5e7657f9a931d1eda0b271f07f1508c8f
Ismael Ors <ismikin@gmail.com>
2018-11-26 10:10:25 +0100

    Linting

--- matcher/main.py
+++ matcher/main.py
@@ -1,4 +1,3 @@
-import time
 import logging
 import csv
 import fire
@@ -10,10 +9,12 @@ logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
+
 def get_all_from_db():
     sound_recordings = get_all_sound_recordings()
     for record in sound_recordings:
         log.info('PRINTING RECORD:{}'.format(record.id))
+
 
 def parse_file_from_cli(file_path):
     with open(file_path, 'r') as f:
@@ -21,7 +22,9 @@ def parse_file_from_cli(file_path):
         for input_record in input_records:
             log.debug('ROW FROM FILE {}'.format(input_record['artist']))
 
+
 def main():
     fire.Fire()
+
 
 main()
