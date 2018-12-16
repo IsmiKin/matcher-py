7b1a3740298d398342c29aec781758d030a4b80e
Ismael Ors <ismikin@gmail.com>
2018-12-16 17:07:00 +0100

    Add table to store scores

--- sql_scripts/init_01_creation.sql
+++ sql_scripts/init_01_creation.sql
@@ -11,3 +11,17 @@ CREATE TABLE "files_processed" (
   "filename" text NOT NULL,
   "file_time_update" timestamp NOT NULL
 );
+
+CREATE TABLE "sound_record_match" (
+  "match_id" text  PRIMARY KEY,
+  "row_number" integer NOT NULL,
+  "artist" text NULL,
+  "title" text NULL,
+  "isrc" text NULL,
+  "duration" integer NULL,
+  "score" integer NOT NULL,
+  "files_processed_FK" text NOT NULL,
+  "sound_recordings_FK" int NOT NULL,
+  FOREIGN KEY ("files_processed_FK") REFERENCES "files_processed" ("hash"),
+  FOREIGN KEY ("sound_recordings_FK") REFERENCES "sound_recordings" ("id")
+);
