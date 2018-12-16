26050200b7122b770bdc88c5aa3e99ce76ed88e0
Ismael Ors <ismikin@gmail.com>
2018-12-16 20:57:08 +0100

    Test for file exists

--- matcher/tests/utils_test.py
+++ matcher/tests/utils_test.py
@@ -1,4 +1,6 @@
-from matcher.utils import validate_isrc
+import pytest
+
+from matcher.utils import validate_isrc, file_exists
 
 
 def test_validate_isrc_valid():
@@ -9,3 +11,16 @@ def test_validate_isrc_valid():
 def test_validate_isrc_not_valid():
     result = validate_isrc('GBAHT12003890')
     assert result is not True
+
+
+def test_file_exists(tmpdir):
+    with pytest.raises(Exception):
+        file_exists("hello.csv")
+
+    file = tmpdir.mkdir("test").join("hello.csv")
+    file.write("col1, col2")
+
+    try:
+        file_exists(file)
+    except Exception:
+        pytest.fail('File exists now, it should not fail')
