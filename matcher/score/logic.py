65943635f65b05229319f9772f944896c48e3951
Ismael Ors <ismikin@gmail.com>
2019-01-18 19:31:19 +0100

    Fix circle ci - try codecov 3

--- matcher/score/logic.py
+++ matcher/score/logic.py
@@ -1,6 +1,4 @@
 from fuzzywuzzy import fuzz
-from .errors import EmptyInputRecord
-
 
 SCORE_METADA_SOUND_RECORDING = [
     {
@@ -24,7 +22,7 @@ SCORE_METADA_SOUND_RECORDING = [
         'final_score_percent': 0.30,
         'optional': False
     },
-    # duration is number, so it will be ignore by now
+    #duration is number, so it will be ignore by now
     # {
     #     'column_name': 'duration',
     #     'type_match': 'duration',
@@ -50,8 +48,8 @@ def match_score_field(field_rules, input_record, db_record):
     #     ratio = match_duration(input_record, db_record)
     # else:
     ratio = field_rules['type_match'](input_record, db_record)
-    # log.info('input_record: {} --- db record {} => ratio: {}'.format(
-    #     input_record, db_record, ratio))
+    # log.info('input_record: {} --- db record {} =>
+    # ratio: {}'.format(input_record, db_record, ratio))
     return ratio
 
 
@@ -59,10 +57,7 @@ def match_score_field(field_rules, input_record, db_record):
 # lose precission
 def calculate_match_score(input_record, db_record):
     final_score = 0.00
-
-    if input_record is None:
-        raise EmptyInputRecord()
-
+    # field_scores = {}
     if db_record is not None:
         for field_score_rules in SCORE_METADA_SOUND_RECORDING:
             field_score = match_score_field(
