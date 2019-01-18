f4f827bc8b84b64318ebea2d8c7d33f3b32d03c1
Ismael Ors <ismikin@gmail.com>
2019-01-18 13:21:24 +0100

    Fix test and linting

--- matcher/utils.py
+++ matcher/utils.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 
 def validate_isrc(input_isrc):
-    pattern = re.compile("^[A-Z]{2}-?\w{3}-?\d{2}-?\d{5}$")
+    pattern = re.compile(r"^[A-Z]{2}-?\w{3}-?\d{2}-?\d{5}$")
     return False if pattern.match(input_isrc) is None else True
 
 
