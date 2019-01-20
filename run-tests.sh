93c7055b485242889897ccc041e32c42d6ec26fe
Ismael Ors <ismikin@gmail.com>
2019-01-20 18:36:23 +0100

    Try to setup coveralls 13 -

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-coverage run --source=matcher/,matcher/score,matcher/data -m pytest --strict -rw --ignore=venv/ --cache-clear --junitxml=test-reports/pytest/junit.xml
+coverage run --source=matcher/,matcher/score,matcher/data -m pytest --strict -rw --ignore=venv/ --cache-clear
 
 coverage_success=$?
 
 coverage report -m --skip-covered
 coverage html -d test-reports/coverage
 coverage xml -o test-reports/coverage.xml
+cp .coverage test-reports/
 
 flake8 matcher
 
