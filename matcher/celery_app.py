1f2a788d1ac62e2b67a76de09b56c2a0b81565a7
Ismael Ors <ismikin@gmail.com>
2019-01-22 14:40:27 +0100

    Try postgresql as be-result and add flower

--- matcher/celery_app.py
+++ matcher/celery_app.py
@@ -4,8 +4,19 @@ from celery import Celery
 app = Celery(
     'matcher',
     broker=os.environ.get('BROKER_URL', 'redis://localhost:6379/0'),
-    backend=os.environ.get('BACKEND_RESULT_URL', 'redis://localhost:6379/0')
+    backend=os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 )
+
+app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],  # Ignore other content
+    result_serializer='json',
+    timezone='Europe/Oslo',
+    enable_utc=True,
+    result_persistent=True
+)
+
+app.conf.database_engine_options = {'echo': True}
 
 
 @app.task
